edee13caff197ebd48f3190032ecf7ae92ab79c0
pedroaraujomedeiros <pedro.araujo.medeiros@gmail.com>
2021-04-10 19:49:02 -0300

    Update API Key

--- CoinTrackerStarter/app/src/main/java/ca/burchill/cointracker/network/CoinNetService.kt
+++ CoinTrackerStarter/app/src/main/java/ca/burchill/cointracker/network/CoinNetService.kt
@@ -13,7 +13,7 @@ import retrofit2.http.Headers
 
 
 private const val BASE_URL = "https://pro-api.coinmarketcap.com/"
-private const val YOUR_API_KEY = "81c565ce-9f80-4d9a-816f-0e216ca23013"
+private const val YOUR_API_KEY = "7d11cee7-9f37-42a5-9ca7-6faf22a516b8"
 
 private val moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
